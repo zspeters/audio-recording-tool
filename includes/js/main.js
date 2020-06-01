1b04682964b58ba08e90256cbcc48e022217bb5f
Zac Peters <zspeters.gp@gmail.com>
2020-06-01 17:06:00 -0600

    Add functionality to new recording buttons, some helper functions

--- includes/js/main.js
+++ includes/js/main.js
@@ -2,16 +2,28 @@ $(document).ready(function() {
 
     let userEmail = '';
     let qPlayer = $('#q-player');
-    let qPlayerPath = 'audio/questions/q';
-    let qPlayerSrc = '';
+    let recordingNow = false;
     let recBtns = $("#container-rec-btns").find(".btn");
     let recBtn = $('#rec-btn');
     let recStopBtn = $('#rec-stop-btn');
     let recPauseBtn = $('#rec-pause-btn');
-    let respPlayer = document.getElementById('resp-player');
+    let respPlayer = $('#resp-player');
+    let respPlayerSrcArr = Array.apply(null, Array(5));
     let prevQBtn = $('#prev-q');
     let qCounter = 0;
     let nextQBtn = $('#next-q');
+
+    const qTextArr = [
+        "How are you feeling today?",
+        "How long have you felt this way?",
+        "How are things going at work?",
+        "Do you smoke cigarettes or cannabis, or drink alcohol? Are you on any medication or any illicit drugs?",
+        "Are you married? Do you have any children? How is your social life?"
+    ];
+    const qPlayerSrcArr = [];
+    for (let i = 1; i < 6; i++) {
+        qPlayerSrcArr.push('audio/questions/q' + i.toString() + '.mp3');
+    }
 
     $('.modal').modal('show');
 
@@ -56,29 +68,15 @@ $(document).ready(function() {
 
     // handle response recording
     recBtn.on("click", function() {
-        let container = $("#container-rec-btns");
-
-        if ($("#rec-btn-icon").hasClass("fa-stop")) {
-            // reenable rec btns when stopping recording
-            container.find(".btn").prop("disabled", false);
-            $("#rec-btn-icon").addClass("fa-circle").removeClass("fa-stop");
-            $("#rec-btn").blur();
-
-            //stop recording
-            if (mediaRecorder.state == "recording") {
-                mediaRecorder.stop();
-            }
-
-        } else {
-
-            // disable all buttons
-            container.find(".btn").prop("disabled", true);
-
-            // start recording
+        //start recording
+        if (!recordingNow) {
+            recordingNow = true;
             let recChunks = [];
+            recBtn.attr('disabled', true);
+            recPauseBtn.attr('disabled', false);
+            recStopBtn.attr('disabled', false);
 
             navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(function(stream) {
-
                 mediaRecorder = new MediaRecorder(stream, { mimeType: 'audio/webm' });
 
                 mediaRecorder.ondataavailable = function(e) {
@@ -88,8 +86,9 @@ $(document).ready(function() {
 
                     if (mediaRecorder.state == 'inactive') {
                         const blob = new Blob(recChunks, { type: 'audio/webm' });
-                        var url = URL.createObjectURL(blob);
-                        $("#resp-player").attr('src', url);
+                        let url = URL.createObjectURL(blob);
+                        respPlayerSrcArr[qCounter] = url;
+                        respPlayer.attr('src', url);
 
                         // upload on navigation to next question if Q1-Q4
                         if (qCounter < 4) {
@@ -101,41 +100,55 @@ $(document).ready(function() {
                         } else {
                             uploadResp(qCounter + 1, blob);
                         }
+
+                        recordingNow = false;
+                        recBtn.attr('disabled', false);
                     }
                 }
 
+                recPauseBtn.on('click', function() {
+                    // pause recording
+                    if (mediaRecorder.state == 'recording') {
+                        mediaRecorder.pause();
+                        recPauseBtn.addClass('active');
+
+                        // resume recording
+                    } else if (mediaRecorder.state == 'paused') {
+                        mediaRecorder.resume();
+                        recPauseBtn.removeClass('active');
+                    }
+                });
+
+                recStopBtn.on('click', function(event) {
+                    // stop recording
+                    if (mediaRecorder.state !== 'inactive') {
+                        mediaRecorder.stop();
+                        recStopBtn.attr('disabled', true);
+                        recPauseBtn.attr('disabled', true);
+                        if (recPauseBtn.hasClass('active')) {
+                            recPauseBtn.removeClass('active');
+                        };
+                        recBtn.attr('disabled', false);
+                    }
+                    $('this').off(event);
+                });
+
                 mediaRecorder.start(1000);
-
             }).catch(alert);
-
-            // update buttons
-            changeButtons('#rec-btn-icon', 'fa-stop', 'fa-circle');
-            $("#rec-btn").prop("disabled", false);
         }
     });
     // end handle response recording
 
     // navigate questions
-    const qTextArr = [
-        "How are you feeling today?",
-        "How long have you felt this way?",
-        "How are things going at work?",
-        "Do you smoke cigarettes or cannabis, or drink alcohol? Are you on any medication or any illicit drugs?",
-        "Are you married? Do you have any children? How is your social life?"
-    ];
-
     nextQBtn.on("click", function() {
-        // enabled 'Previous Question' button when not on Q1
+        // enable 'Previous Question' button when not on Q1
         if (0 == qCounter) {
             prevQBtn.attr('disabled', false);
             prevQBtn.removeClass('no-click');
         }
         if ((4 > qCounter) && (qCounter >= 0)) {
             qCounter++;
-            // change question audio source
-            qPlayerSrc = qPlayerPath + (qCounter + 1).toString() + '.mp3';
-            console.log(qPlayerSrc);
-            qPlayer.attr('src', qPlayerSrc);
+            changeSrc();
             // change nav button text
             $('#q-body').text(qTextArr[qCounter]);
             $('#q-counter').text(`${qCounter + 1} of 5`);
@@ -148,17 +161,14 @@ $(document).ready(function() {
     });
 
     prevQBtn.on("click", function() {
-        // disabled '< Previous Question' button on Q1
+        // disable '< Previous Question' button on Q1
         if (1 == qCounter) {
             prevQBtn.attr('disabled', true);
             prevQBtn.addClass('no-click');
         }
         if ((5 > qCounter) && (qCounter > 0)) {
             qCounter--;
-            // change question audio source
-            qPlayerSrc = qPlayerPath + (qCounter + 1).toString() + '.mp3';
-            console.log(qPlayerSrc);
-            qPlayer.attr('src', qPlayerSrc);
+            changeSrc();
             // change nav button text
             $('#q-body').text(qTextArr[qCounter]);
             $('#q-counter').text(`${qCounter + 1} of 5`);
@@ -185,19 +195,32 @@ $(document).ready(function() {
     }
     // end helper - upload responses
 
+    // helper to change player audio source
+    function changeSrc() {
+        // change question audio source
+        qPlayer.attr('src', qPlayerSrcArr[qCounter]);
+        // change response audio source
+        if (typeof respPlayerSrcArr[qCounter] == 'string') {
+            respPlayer.attr('src', respPlayerSrcArr[qCounter]);
+        } else {
+            respPlayer.attr('src', '#');
+        }
+    };
+    // end helper - player src change
+
     // helper to manage buttons after response audio finishes playback passively
     respPlayer.onended = function() {
-            respPlayer.load();
-            recBtns.prop("disabled", false);
-            recBtns.removeClass('active');
-            changeButtons("#rec-play-btn-icon", 'fa-play', 'fa-pause');
-        }
-        // end helper - button management
+        respPlayer.load();
+        recBtns.prop("disabled", false);
+        recBtns.removeClass('active');
+        changeButtons("#rec-play-btn-icon", 'fa-play', 'fa-pause');
+    };
+    // end helper - button management
 
     // helper to change button icons
     function changeButtons(btnID, add, remove) {
         $(btnID).addClass(add).removeClass(remove);
-    }
+    };
     // end helper - icon change
 
 });
